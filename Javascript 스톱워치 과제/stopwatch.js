df796399cc0e61a490b36fbcaf044deca9660740
wldy4627 <wldy4627@gmail.com>
2024-07-05 21:23:26 +0900

    [stopwatch] 구간 기록

--- Javascript 스톱워치 과제/stopwatch.js
+++ Javascript 스톱워치 과제/stopwatch.js
@@ -1,21 +1,28 @@
 // interval을 설정하기 위한 변수
 let intervalState;
+// 현재 시간을 위한 변수
 let second = 0;
 let millisecond = 0;
+// 기록용 시간을 위한 변수
+let recordSecond;
+let recordMillisecond;
 // 스탑워치가 stop이거나 reset일 때 true
 let stopWatchState = false;
 // <div class="time"></div>
 const timeContainer = document.querySelector('.time');
-// <p class="second"></p>
+// <p class="second"></p> && 초기화
 const secondText = document.createElement('p');
 secondText.className = 'second';
 secondText.innerText = '00';
 // <p></p>
 let textNode = document.createTextNode(':');
-// <p class="millisecond"></p>
+// <p class="millisecond"></p> && 초기화
 const millisecondText = document.createElement('p');
 millisecondText.className = 'millisecond';
 millisecondText.innerText = '00';
+// <div class="record__bottom"></div>
+const records = document.querySelector('.record__bottom');
+
 
 const startBtn = document.getElementById('start');
 startBtn.addEventListener("click", () => {
@@ -29,6 +36,36 @@ startBtn.addEventListener("click", () => {
 const stopBtn = document.getElementById('stop');
 stopBtn.addEventListener("click", () => {
    console.log("pushed stop!!");
+   // 기록하기
+   if (second <= 9) {
+      recordSecond = '0' + second;
+   } else {
+      recordSecond = second;
+   }
+   if (millisecond <= 9) {
+      recordMillisecond = '0' + millisecond;
+   } else {
+      recordMillisecond = millisecond;
+   }
+   // <div class="record"></div>
+   const record = document.createElement('div');
+   record.className = 'record';
+   // <button class="button__circle"></button>
+   const circleBtn = document.createElement('button');
+   circleBtn.className = 'bottom__circle';
+   // <i class="fa-regular fa-circle"></i>
+   const nonCheckedBox = document.createElement('i');
+   nonCheckedBox.className = 'fa-regular fa-circle';
+   circleBtn.appendChild(nonCheckedBox);
+   // <p>"기록"</p>
+   let recordText = document.createElement('p');
+   recordText.innerText = recordSecond + ':' + recordMillisecond;
+   record.appendChild(nonCheckedBox);
+   record.appendChild(recordText);
+   records.appendChild(record);
+
+   console.log(recordText.innerText);
+   // setInterval 멈추기
    clearInterval(intervalState);
    stopWatchState = false;
 })
